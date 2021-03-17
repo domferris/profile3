8e8116335868f366bb8932ee4dceb2a8d1515cf9
Dominic Ferris <contactdomferris@gmail.com>
2021-03-17 01:26:09 -0500

    refine bg color change transitions

--- src/_bundle/main.js
+++ src/_bundle/main.js
@@ -10,17 +10,17 @@ const scroll = new LocomotiveScroll({
 scroll.on("scroll", (args) => {
   const body = document.querySelector("body");
   const softBrown = "#C9C0B5";
-  const softMaroon = "#B8AFAF";
   const softOlive = "#C1C2B1";
+  const softMaroon = "#B8AFAF";
   const yellowGray = "#C0C0BC";
 
   // change bg color on scroll
   if (typeof args.currentElements["home"] === "object") {
     const progress = args.currentElements["home"].progress;
 
-    if (progress > 0.95) {
+    if (progress > 0.925) {
       // why section downward scroll bg color change
-      body.style.backgroundColor = softMaroon;
+      body.style.backgroundColor = softOlive;
     } else {
       // home section upward scroll bg color change
       body.style.backgroundColor = softBrown;
@@ -28,22 +28,22 @@ scroll.on("scroll", (args) => {
   } else if (typeof args.currentElements["why"] === "object") {
     const progress = args.currentElements["why"].progress;
 
-    if (progress > 0.95) {
+    if (progress > 0.94) {
       // projects section downward scroll bg color change
-      body.style.backgroundColor = softOlive;
+      body.style.backgroundColor = softMaroon;
     } else {
       // why section upward scroll bg color change
-      body.style.backgroundColor = softMaroon;
+      body.style.backgroundColor = softOlive;
     }
   } else if (typeof args.currentElements["projects"] === "object") {
     const progress = args.currentElements["projects"].progress;
 
-    if (progress > 0.95) {
+    if (progress > 0.93) {
       // connect section downward scroll bg color change
       body.style.backgroundColor = yellowGray;
     } else {
       // projects section upward scroll bg color change
-      body.style.backgroundColor = softOlive;
+      body.style.backgroundColor = softMaroon;
     }
   }
 });
